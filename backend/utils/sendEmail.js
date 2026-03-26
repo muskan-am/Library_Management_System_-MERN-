3a0e047416d73314c84bdac32d97fa7ed83c558f
Muskan Kesharwani <muskankesharwani@sigma.in>
2026-03-27 00:39:53 +0530

    Initial commit

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async ({ email, subject, message }) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",   // ✅ only this
+    port:465,
+    secure: true, //Required
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  try{
+
+    await transporter.sendMail({
+      from: process.env.SMTP_MAIL,
+      to: email,
+      subject,
+      html: message,
+    });
+  } catch(error) {
+    console.log("Email Error: ", error); 
+    throw error;
+  }
+};
