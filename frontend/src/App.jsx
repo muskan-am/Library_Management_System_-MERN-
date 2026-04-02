4e776f31b54dd97de3ebe8b1a95e72e20648f596
Muskan Kesharwani <muskankesharwani@sigma.in>
2026-04-02 21:27:10 +0530

    add Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import OTP from "./pages/OTP";
+import Register from "./pages/Register";
+import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./store/slices/authSlice";
+import { fetchAllUsers } from "./store/slices/userSlice";
+
+
+const App = () => {
+  const { user, isAuthenticated } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+    if (isAuthenticated && user?.role === "Admin") {
+      //console.log("THE LOGGED IN USER IS AN ADMIN")
+      dispatch(fetchAllUsers());
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/otp-verification/:email" element={<OTP />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+      </Routes>
+      <ToastContainer theme="dark" />
+    </Router>
+  )
+};
+
+export default App;
