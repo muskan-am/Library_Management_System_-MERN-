c3b87729d9a0914ac790acd1a959484037deab3d
Muskan Kesharwani <muskankesharwani63@gmail.com>
2026-04-14 00:14:42 +0530

    changes cookies

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -4,9 +4,12 @@ export const sendToken = (user, statusCode, message, res) => {
      .status(statusCode)
      .cookie("token", token, {
         expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 *60 * 60 * 1000
-        ), 
+            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
         httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        path: "/",
     })
     .json({
         success: true,
