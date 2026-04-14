1f48cb7aa417d3ccc4fb154f2d02f7045216ae9f
Muskan Kesharwani <muskankesharwani63@gmail.com>
2026-04-15 00:06:53 +0530

    update sendEmailfile

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,25 +2,24 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",   // ✅ only this
-    port:465,
-    secure: true, //Required
+    host: "smtp.gmail.com",   
+    port: 587,              
+    secure: false,         
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
     },
   });
 
-  try{
-
+  try {
     await transporter.sendMail({
       from: process.env.SMTP_MAIL,
       to: email,
       subject,
       html: message,
     });
-  } catch(error) {
-    console.log("Email Error: ", error); 
+  } catch (error) {
+    console.log("Email Error: ", error);
     throw error;
   }
 };
